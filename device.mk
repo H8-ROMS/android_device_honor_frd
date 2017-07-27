2cec8908e76342aea1aa9d6fa4b45ca5892c19ce
Muhammad Fahad Baig <fadi@fahadbaig.com>
2017-07-27 08:10:32 -0700

    re-target device to mokee

--- device.mk
+++ device.mk
@@ -72,10 +72,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
-
-#CMActions
-PRODUCT_PACKAGES += \
-	CMActions
 	
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
